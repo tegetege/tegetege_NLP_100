ee76a957834d26f58ff9c5184b106d2c82cb84f3
tegetege <t.tose@uec.ac.jp>
2018-02-07 19:10:56 +0900

    第19まで終了
    第19にコメントした"to do"から明日はスタート

--- 2/section2.py
+++ 2/section2.py
@@ -4,7 +4,7 @@ class Section_2():
 		self.input_file = 'hightemp.txt' #読み込むtxtファイル
 
 
-	#指定ファイルを読み込み、ファイルの中身を返す
+	#指定ファイル'hightemp.txt'を読み込み、生データを返す
 	def file_read(self):
 		f = open(self.input_file) #ファイルオープン
 		data = f.read()			  #データ読み込み
@@ -79,7 +79,7 @@ class Section_2():
 		fout = open('marge_col1_col2.txt','w')
 		#フォーマット関数でデータを形成して書き込む
 		for i in range(len(data_1_list)):
-			row = '{}\t{}'.format(data_1_list[i],data_2_list[i])
+			row = '{}\t{}'.format(data_1_list[i],data_2_list[i]) #フォーマット関数
 
 			print(row,file=fout,end='\n')
 		fout.close()
@@ -97,6 +97,7 @@ class Section_2():
 		for i in range(int(line_num)):
 			print(data_list[i])
 
+
 	#末尾のN行を出力
 	def ss5(self):
 		line_num = input('末尾から何行を出力する？')
@@ -106,12 +107,117 @@ class Section_2():
 		data_list = data.split('\n')
 
 		for i in range(1,int(line_num)+1):
-			i = '-' + str(i)
-			print(i)
+			i = '-' + str(i)  #マイナスをつけて、リストの逆回しを実現
 			print(data_list[int(i)])
 
 
+	#ファイルをN分割する
+	def ss6(self):
+		'''	
+		・入力ファイルを"hightemp.txt"とする
 
+		・リストの個数が割り切れない時は、初めの方の分割個数を多くすることで調節
+		・"readline()"は毎行ずつの処理なので、リストの個数で管理する必要がない
+		　最後に忘れずにclose()
+
+		'''
+		division_num = input('ファイルを何分割する？')
+		count = 0
+		
+		#ファイルデータを取得しリスト化
+		data = self.file_read()
+		list_data = data.split('\n')
+		
+		#一つのファイル内のリストの個数を計算
+		num_list_each_division = len(list_data) / int(division_num)
+
+		f = open('hightemp.txt')
+
+		#(※)int型かどうかの判定で、リストの個数が割り切れるかの判定をする
+		if num_list_each_division == int(num_list_each_division): #リストが割り切れる
+			
+			for i in range(int(division_num)) : #分割数だけ繰り返す
+				print(i,'分割')
+				for j in  range(int(num_list_each_division)): #分割内の個数分繰り返す
+					print(f.readline())
+
+		else: #リストが割り切れず、余り処理が必要
+			
+			surplus_num = len(list_data) % int(division_num) #余剰
+
+			for i in range(int(division_num)) :
+				print(i,'分割')
+				if i < int(surplus_num) : #余剰分だけ、　分割内個数を+1でリストを出力
+					for j in range(int(num_list_each_division)+1): #+1
+						print(f.readline())
+				else:
+					for j in range(int(num_list_each_division)):
+						print(f.readline())
+		
+		f.close() #忘れずにファイルクローズ
+
+
+	#1列目の文字列の異なり
+	def ss7(self):
+		ans_set = ()
+
+		data = self.file_read()
+		lines = data.split('\n') 
+
+		for i in range(len(lines)):
+			chr_list = list(lines[i].split('\t')[0]) #一文字ずつリスト化できる
+			for j in range(len(chr_list)):
+
+				ans_set = set(ans_set) | set(chr_list)        #和集合
+				#ans_set = set(ans_set).union(set(chr_list))  #こっちでもOK
+
+		print(ans_set)
+
+
+	#各行を3コラム目の数値の降順にソート
+	def ss8(self):
+		row_3 = []
+
+		data = self.file_read()
+		lines = data.split('\n')
+
+		for i in range(len(lines)):
+			row_3.append(lines[i].split('\t')[2])
+
+		row_3.sort(reverse = True) #ここでprintしてもNoneが帰ってくるだけ
+		#"reverse = Trueを消せば昇順になる"
+		print(row_3)
+
+	#各行の1コラム目の文字列の出現頻度を降順に表示
+	def ss9(self):
+		#辞書で一つずつ数える
+
+		row_1 = {} #dic
+
+		data = self.file_read()
+		lines = data.split('\n')
+
+		for i in range(len(lines)):
+			'''
+			[to do!!!]
+			このクソコードをどうにかする。
+			
+			'''
+			#辞書のキーを見て、同一のキーがあるかどうかの判定
+			if row_1.get(lines[i].split('\t')[0]) == None :
+				print('同じものなし')
+				#要素を+1
+				add_dic = {lines[i].split('\t')[0]:1}
+				row_1.update(add_dic)
+			else:
+				print('同じものあり')
+				
+				add_dic = {lines[i].split('\t')[0]:row_1.get(lines[i].split('\t')[0]) +1}
+				row_1.update(add_dic)
+
+
+
+		print(row_1)
 
 num = input('サブセクション番号入力:')
 do  = Section_2()
