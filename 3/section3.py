52cc81fdf9c13857b112c9f99a860687b0c04328
tegetege <t.tose@uec.ac.jp>
2018-02-12 15:29:34 +0900

    第22まで終了 #tegetege_work
    jsonからデータを出力するのにだいぶ時間がかかってしまったが、
    それ以降はreの処理のみなのでスラスラと...
    
    section3.py の83行でエラーが出る状態でコミットしてしまった...

--- 3/section3.py
+++ 3/section3.py
@@ -1,27 +1,86 @@
+import json
+
+
 class Section_3():
 
+	#ss1-ss9で利用 jsonを読んでデータを返す
+	def read_json(self):
+
+		f = open('jawiki-country.json','r')
+
+		for count,line in enumerate(f): 
+			data_json = json.loads(line)
+			if data_json['title'] == 'イギリス':
+				text_data = data_json['text']
+				break
+		f.close()		#ファイルクローズをお忘れなく	
+		return text_data
+
+	#JSONデータの読み込み
+	#read_jsonとやってることは同じ
 	def ss0(self):
-		import json
+
 
 		#jsonファイルは一行ずつ読まないと怒られる...
-
-		temp = {}
 		f = open('jawiki-country.json','r')
-		
-		i = 0
 
-
-		for num,line in enumerate(f): 
-
-			temp[line] = json.loads(line)
-			print(temp[line])
-
-			i+=1			
-
+		for count,line in enumerate(f): 
+			'''
+			enumerate()はカウントと値を返すので、
+			for文の引数も"num,line"となる
+			'''
+			data_json = json.loads(line)
+			if data_json['title'] == 'イギリス':
+				print(data_json['text'])
 		f.close()
-		#print()
 
+	#カテゴリ名を含む行を抽出
+	def ss1(self):
+		'''
+		>[[Category:英連邦王国|*]]
+		これを抜き出す
+		'''
+		import re 
 
+		#全文データを取得
+		data = self.read_json()
+		lines = data.split('\n') #リスト化
+
+		pattern = r'\[\[Category:' #正規表現で探す時のパターン
+		for i in range(len(lines)):
+			matchOB = re.match(pattern,lines[i])
+			if matchOB :
+				print(lines[i])
+
+	#カテゴリ名の抽出
+	def ss2(self):
+		'''
+		>[[Category:英連邦王国|*]]
+		これから、以下のように抜き出す
+		>英連邦王国|*
+		'''
+		import  re
+
+		#全文データを取得
+		data = self.read_json()
+		lines = data.split('\n') #リスト化
+
+		pattern1 = r'\[\[Category:' #正規表現で探す時のパターン
+		pattern2 = r'\S*'
+
+		for i in range(len(lines)):
+			matchOB_1 = re.match(pattern1,lines[i])
+			if matchOB_1 :
+				matchOB_2 = re.match(pattern2,lines[i])
+				if matchOB_2 :
+					#pattern1のマッチ部分を抜き出す
+					re_pattern1 = matchOB_1.group()
+					#いらない部分をreplaceで削除
+					category_name = matchOB_2.group().replace(re_pattern1,'').replace(']]','')
+					print(category_name)
+
+	#セクション構造
+	def ss3(self):
 
 
 
