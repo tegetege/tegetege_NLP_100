fa56bfe2872c1a86b064c1756ea31432b9f4169b
tegetege <t.tose@uec.ac.jp>
2018-03-05 18:51:26 +0900

    第31,32 完了 #tegetege_work

--- 4/section4.py
+++ 4/section4.py
@@ -12,6 +12,11 @@ class Section_4():
 	辞書にはオプションで、"mecab-ipadic-NEologd"を利用
 	'''
 
+	def __init__(self):
+		#クラス内でデータを格納するリストを管理
+		self.word_list = [] #単語部分のリスト
+		self.morphemes = [] #形態素部分のリスト
+
 	def ss0(self):
 		'''
 		こんな感じで表現したい
@@ -22,15 +27,13 @@ class Section_4():
 		  pos1: '一般'
 		},
 		'''
-		
+
 		'''
 		########
 		データ形成をする上でエラーが出るため、neko.txt.mecabの最後の
 		空白行を消去した。
 		########
 		'''
-		word_list = [] #単語部分のリスト
-		morphemes = [] #形態素部分のリスト
 
 		with open("neko.txt.mecab", "r") as f:
 			data = f.read()
@@ -41,20 +44,53 @@ class Section_4():
 			data.remove('EOS')
 		for i in range(len(data)):
 			data[i]= data[i].split('\t') #データ形成 2次元配列化
-			word_list.append(data[i][0]) #単語の部分をリスト化
-			if len(data[i]) > 1:
-				morphemes.append(data[i][1].split(',')) #形態素部分をリスト化
+			self.word_list.append(data[i][0]) #単語の部分をリスト化
+			if len(data[i]) > 1: #エラー回避
+				self.morphemes.append(data[i][1].split(',')) #形態素部分をリスト化
 			else:
 				pass
 		
-		for i in range(len(word_list)):
+		for i in range(len(self.word_list)):
 			print(i)
-			print('surface',':',word_list[i])
-			print('base',':',morphemes[i][6])
-			print('pos',':',morphemes[i][0])
-			print('pos1',':',morphemes[i][1])
+			print('surface',':',self.word_list[i])
+			print('base',':',self.morphemes[i][6])
+			print('pos',':',self.morphemes[i][0])
+			print('pos1',':',self.morphemes[i][1])
 			print('-------------------------')	
 		
+	def make_data(self):
+
+		with open("neko.txt.mecab", "r") as f:
+			data = f.read()
+
+		data = data.split('\n') #データ形成 : リスト化
+		#データ形成 : 不要なEOS要素を削除
+		while 'EOS' in data:
+			data.remove('EOS')
+		for i in range(len(data)):
+			data[i]= data[i].split('\t') #データ形成 2次元配列化
+			self.word_list.append(data[i][0]) #単語の部分をリスト化
+			if len(data[i]) > 1:
+				self.morphemes.append(data[i][1].split(',')) #形態素部分をリスト化
+			else:
+				pass
+
+	def ss1(self):
+		self.make_data() #データ生成
+		for i in range(len(self.word_list)):
+			if self.morphemes[i][0] =='動詞':
+				print('-------------------------')
+				print('surface',':',self.word_list[i])
+				print('pos',':',self.morphemes[i][0])
+
+	def ss2(self):
+		self.make_data() #データ生成
+		for i in range(len(self.word_list)):
+			if self.morphemes[i][0] =='動詞':
+				print('-------------------------')
+				print('surface',':',self.word_list[i])
+				print('base',':',self.morphemes[i][6])
+
 
 num = input('サブセクション番号入力:')
 do  = Section_4()
