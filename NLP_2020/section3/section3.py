6d8361150c483eda248fde45d4dc4569f3dc0bbf
tegetege <24471423+tegetege@users.noreply.github.com>
2020-08-13 00:05:24 +0900

    add 24

--- NLP_2020/section3/section3.py
+++ NLP_2020/section3/section3.py
@@ -12,7 +12,7 @@ class Section_3:
         f = open('jawiki-country.json', 'r')
         data = f.read()
         f.close()
-        data = list(map(str,data.split('\n')))
+        data = list(map(str, data.split('\n')))
 
         for i in range(len(data)):
             data_json = json.loads(data[i])
@@ -31,7 +31,7 @@ class Section_3:
 
         pattern = r'\[\[Category:'
         for i in range(len(target_list)):
-            match = re.match(pattern,target_list[i])
+            match = re.match(pattern, target_list[i])
             if match:
                 print(target_list[i])
     
@@ -40,7 +40,7 @@ class Section_3:
         target_list =  self.target.split('\n')
         pattern = r'\[\[Category:'
         for i in range(len(target_list)):
-            match = re.match(pattern,target_list[i])
+            match = re.match(pattern, target_list[i])
             if match:
                 print(target_list[i].replace('[[Category:', '').replace('|*', '').replace('|元', '').replace(']]', ''))
 
@@ -48,11 +48,23 @@ class Section_3:
         self.read_json()
         target_list = self.target.split('\n')
         for i in range(len(target_list)):
-            me = re.match(r'=+\w+=+',target_list[i])
+            me = re.match(r'=+\w+=+', target_list[i])
             if me != None:
                 m = re.match(r'=+',me[0])
-
                 print(me[0].replace('=', ''), ':', int(len(m[0])-1))
+
+    def ss4(self):
+        # [[File:Dayak2 2008New.jpg
+        # [[ファイル:All Gizah Pyramids.jpg
+        # こういうのを抜き出したい
+        # 参考:https://qiita.com/moriwo/items/badc6432cb925676089a#24-%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E5%8F%82%E7%85%A7%E3%81%AE%E6%8A%BD%E5%87%BA
+        self.read_json()
+        target_list = self.target.split('\n')
+
+        for i in range(len(target_list)):
+            match = re.findall(r'\[\[(ファイル|File):([^]|]+?)(\|.*?)+\]\]', target_list[i])
+            if match:
+                print('match:', match[0][1])
     
 
 num = input('サブセクション番号入力:')
