f69f9aca26fecc34cc577b7d602676bc426cc506
tegetege <24471423+tegetege@users.noreply.github.com>
2020-09-13 09:45:11 +0900

    add 37,   commit at Yamanakako Observatory

--- NLP_2020/section4/section4.py
+++ NLP_2020/section4/section4.py
@@ -9,6 +9,7 @@ class Section_4:
         self.target = list()
         self.list_formated = list()
         self.word_freq_sorted = list()
+        self.neko_cooccurrence_freq_sorted = list()
     
     # neko.txt を mecab にかけた neko.txt.mecab を作成
     # 解析結果
@@ -62,6 +63,21 @@ class Section_4:
                     word_freq[self.list_formated[i][j]['surface']] = 1
         
         self.word_freq_sorted = sorted(word_freq.items(), key=lambda x:x[1], reverse=True)
+    
+    def neko_cooccurrence_freq(self):
+        neko_cooccurrence_freq = dict()
+        for i in range(len(self.list_formated)):
+            for j in range(len(self.list_formated[i])):
+                if (self.list_formated[i][j]['surface'] in '猫'):
+                    for k in range(len(self.list_formated[i])):
+                        if (self.list_formated[i][k]['surface'] in neko_cooccurrence_freq):
+                            neko_cooccurrence_freq[self.list_formated[i][k]['surface']] += 1
+                        else:
+                            neko_cooccurrence_freq[self.list_formated[i][k]['surface']] = 1
+                else:
+                    break
+        self.neko_cooccurrence_freq_sorted = sorted(neko_cooccurrence_freq.items(), key=lambda x:x[1], reverse=True)
+        self.neko_cooccurrence_freq_sorted.pop(0)
     
     def ss0(self):
         self.neko_format()
@@ -120,14 +136,32 @@ class Section_4:
     def ss6(self):
         self.neko_format()
         self.make_word_freq()
+
         # 上位10のmapを取り出して、そのkey,valueのリストを作成する
         target_key = list(map(lambda x: x[0], self.word_freq_sorted[:10]))
         target_value = list(map(lambda x: x[1], self.word_freq_sorted[:10]))
         rank = list(range(10))
 
-
+        # グラフ表示
         plt.bar(rank, target_value, tick_label=target_key)
         plt.title(' 単語の出現頻度 上位10 ')
+        plt.show()
+    
+    def ss7(self):
+        self.neko_format()
+
+        # "猫" との共起頻度(cooccurrence freq)を求める
+        self.neko_cooccurrence_freq()
+        # print(self.neko_cooccurrence_freq_sorted)
+        
+        # 上位10を取り出して、そのkey,valueのリストを作成する
+        target_key = list(map(lambda x: x[0], self.neko_cooccurrence_freq_sorted[:10]))
+        target_value = list(map(lambda x: x[1], self.neko_cooccurrence_freq_sorted[:10]))
+        rank = list(range(10))
+        
+        # グラフ表示
+        plt.bar(rank, target_value, tick_label=target_key)
+        plt.title(' "猫" との共起頻度 上位10 ')
         plt.show()
 
 
